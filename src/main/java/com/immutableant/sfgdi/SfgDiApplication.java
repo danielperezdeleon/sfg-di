bea2f327bc0e4157e4a916f8b8c124e56cde68c1
Daniel Perez de Leon <daniel.perezdeleon@immutableant.com>
2022-09-27 18:02:59 -0700

    Added more dependency injection examples now using Spring: property, setter and constructor

--- src/main/java/com/immutableant/sfgdi/SfgDiApplication.java
+++ src/main/java/com/immutableant/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package com.immutableant.sfgdi;
 
+import com.immutableant.sfgdi.controllers.ConstructorInjectedController;
 import com.immutableant.sfgdi.controllers.MyController;
+import com.immutableant.sfgdi.controllers.PropertyInjectedController;
+import com.immutableant.sfgdi.controllers.SetterInjectedController;
 import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +22,26 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
+
+		System.out.println("------ Property based");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------ Setter based");
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------ Constructor based");
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreeting());
+
+
 	}
 
 }
--- src/main/java/com/immutableant/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/immutableant/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package com.immutableant.sfgdi.controllers;
 
 import com.immutableant.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
   private final GreetingService greetingService;
 
